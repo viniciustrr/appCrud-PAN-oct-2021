1554ea061377a13efb7caca1c6e970d781a09db9
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-10-09 17:42:34 -0300

    Crud system using SQL and Postman

---
+++ src/main/java/com/crud/model/MaintenanceModel.java
@@ -0,0 +1,73 @@
+package com.crud.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MaintenanceModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private Date dia;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private double valor;
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDia() {
+		return dia;
+	}
+
+	public void setDia(Date dia) {
+		this.dia = dia;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+}
